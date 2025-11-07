18fe9244cb80512d3eb47d5323b6364aabc634ce
Rahul Arade <araderahul9196@gmail.com>
2025-11-07 09:38:46 +0530

    Added Style

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,18 +2,17 @@ import prisma from "@repo/db/client";
 import styles from "./page.module.css";
 
 export default async function Home() {
-
-  const user = await prisma.user.findFirst();
-  return (
-    <div>
-      <div>
-    Username: 
-      {user?.username}
-      </div>
-      <div>
-    Password: 
-      {user?.password}
-      </div>
-    </div>
-  );
+    const user = await prisma.user.findFirst();
+    return (
+        <div style={{ textAlign: "center" }}>
+            <div style={{ color: "yellow" }}>
+                Username:
+                {user?.username}
+            </div>
+            <div style={{ color: "yellow" }}>
+                Password:
+                {user?.password}
+            </div>
+        </div>
+    );
 }
